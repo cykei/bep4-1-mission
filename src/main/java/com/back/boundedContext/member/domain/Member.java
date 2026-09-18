6878392d827c2c7808fb9ff663821a9c2c67de63
cykei <kelle111@naver.com>
2026-09-18 12:43:04 +0900

    0017

--- src/main/java/com/back/boundedContext/member/domain/Member.java
+++ src/main/java/com/back/boundedContext/member/domain/Member.java
@@ -11,7 +11,7 @@ import lombok.NoArgsConstructor;
 @Table(name = "MEMBER_MEMBER")
 @NoArgsConstructor
 @Getter
-public class Member extends BaseIdAndTime {
+public class Member extends SourceMember {
     @Column(unique = true)
     private String username;
     private String password;
@@ -19,12 +19,11 @@ public class Member extends BaseIdAndTime {
     private int activityScore;
 
     public Member(String username, String password, String nickname) {
-        this.username = username;
-        this.password = password;
-        this.nickname = nickname;
+        super(username, password, nickname);
     }
 
     public int increaseActivityScore(int amount) {
-        return this.activityScore += amount;
+        setActivityScore(getActivityScore() + amount);
+        return getActivityScore();
     }
 }
--- src/main/java/com/back/boundedContext/post/domain/PostMember.java
+++ src/main/java/com/back/boundedContext/post/domain/PostMember.java
@@ -1,7 +1,6 @@
 package com.back.boundedContext.post.domain;
 
-import com.back.global.jpa.entity.BaseIdAndTime;
-import com.back.global.jpa.entity.BaseIdAndTimeManual;
+import com.back.boundedContext.member.domain.ReplicaMember;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
@@ -12,11 +11,8 @@ import lombok.NoArgsConstructor;
 @Table(name = "POST_MEMBER")
 @NoArgsConstructor
 @Getter
-public class PostMember extends BaseIdAndTimeManual {
-    // 이렇게 다 복제한다니? Member 테이블 하나더 만드는거나 다름없군. 동기화 문제가 발생하겠는걸.
-    @Column(unique = true)
-    private String username;
-    private String password;
-    private String nickname;
-    private int activityScore;
+public class PostMember extends ReplicaMember {
+    public PostMember(String username, String password, String nickname) {
+        super(username, password, nickname);
+    }
 }
