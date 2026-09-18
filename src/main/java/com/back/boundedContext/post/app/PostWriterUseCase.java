3d1ca67e1e7b94af52172c4727bb37f4e8b94c50
cykei <kelle111@naver.com>
2026-09-18 12:24:44 +0900

    0014

--- src/main/java/com/back/boundedContext/post/app/PostWriterUseCase.java
+++ src/main/java/com/back/boundedContext/post/app/PostWriterUseCase.java
@@ -2,6 +2,7 @@ package com.back.boundedContext.post.app;
 
 import com.back.boundedContext.member.app.MemberFacade;
 import com.back.boundedContext.member.domain.Member;
+import com.back.boundedContext.member.out.MemberApiClient;
 import com.back.boundedContext.post.domain.Post;
 import com.back.boundedContext.post.out.PostRepository;
 import com.back.global.eventPublisher.EventPublisher;
@@ -16,7 +17,7 @@ import org.springframework.stereotype.Service;
 public class PostWriterUseCase {
     private final PostRepository postRepository;
     private final EventPublisher eventPublisher;
-    private final MemberFacade memberFacade;
+    private final MemberApiClient memberApiClient;
 
     public RsData<Post> write(Member author, String title, String content) {
         Post post = postRepository.save(new Post(author, title, content));
@@ -25,7 +26,7 @@ public class PostWriterUseCase {
                         new PostDto(post)
                 )
         );
-        String randomSecureTip = memberFacade.getRandomSecureTip();
+        String randomSecureTip = memberApiClient.getRandomSecureTip();
 
         // resultCode 를 문자열로 넣다니...
         return new RsData<>(
