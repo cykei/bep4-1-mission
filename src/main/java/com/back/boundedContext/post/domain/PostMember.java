d667e2498b199ddad54ffe6b539132ca2dad0a1c
cykei <kelle111@naver.com>
2026-09-18 12:29:28 +0900

    0015

---
+++ src/main/java/com/back/boundedContext/post/domain/PostMember.java
@@ -0,0 +1,21 @@
+package com.back.boundedContext.post.domain;
+
+import com.back.global.jpa.entity.BaseIdAndTime;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "POST_MEMBER")
+@NoArgsConstructor
+@Getter
+public class PostMember extends BaseIdAndTime {
+    // 이렇게 다 복제한다니? Member 테이블 하나더 만드는거나 다름없군. 동기화 문제가 발생하겠는걸.
+    @Column(unique = true)
+    private String username;
+    private String password;
+    private String nickname;
+    private int activityScore;
+}
